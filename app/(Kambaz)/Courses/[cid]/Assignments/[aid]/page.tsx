3f1f2b7595c26e68cda00d90a4ba11e0b493607c
Ashwin <ash.sponge@email.com>
2025-09-22 17:59:38 -0400

    initial

---
+++ app/(Kambaz)/Courses/[cid]/Assignments/[aid]/page.tsx
@@ -0,0 +1,121 @@
+export default function AssignmentEditor() {
+  return (
+    <div id="wd-assignments-editor">
+      <label htmlFor="wd-name">Assignment Name</label>
+      <input id="wd-name" defaultValue="A1 - ENV + HTML" /><br /><br />
+      <textarea
+        id="wd-description"
+        defaultValue="The assignment is available online Submit a link to the landing page of"
+      />
+      <br />
+      <table>
+        <tbody>
+          <tr>
+            <td align="right" valign="top">
+              <label htmlFor="wd-points">Points</label>
+            </td>
+            <td>
+              <input id="wd-points" defaultValue={100} />
+            </td>
+          </tr>
+          <tr>
+            <td align="right" valign="top">
+              <label htmlFor="wd-display-grade-as">Display Grade As</label>
+            </td>
+            <td>
+              <select id="wd-display-grade-as" defaultValue="Percentage">
+               <option value="Percentage">Percentage</option>
+               </select>
+            </td>
+          </tr>
+          <tr>
+            <td align="right" valign="top">
+              <label htmlFor="wd-submission-type">Submission Type</label>
+            </td>
+            <td>
+               <select id="wd-submission-type" defaultValue="Online">
+               <option value="Online">Online</option>
+               <option value="Offline">Offline</option>
+               </select>
+            </td>
+          </tr>
+           <tr>
+            <td align="right" valign="top">
+              <label htmlFor="wd-text-entry">Online entry options</label>
+            </td>
+            <td>
+               <select id="wd-submission-type" defaultValue="Online">
+               <option value="Online">Online</option>
+               <option value="Offline">Offline</option>
+               </select>
+            </td>
+          </tr>
+          
+          <tr>
+            <td align="right" valign="top">
+              <label htmlFor="wd-text-entry">Online entry options</label>
+            </td>
+            <td>
+
+<input type="radio" name="radio-text-entry" id="wd-text-entry"/>
+<label htmlFor="wd-text-entry">Text Entry</label><br />
+
+<input type="radio" name="radio-website-url" id="wd-website-url"/>
+<label htmlFor="wd-website-url">Webite URL</label><br />
+
+<input type="radio" name="radio-media-recordings" id="wd-media-recordings"/>
+<label htmlFor="wd-radio-scifi">Media Recordings</label><br />
+
+<input type="radio" name="radio-student-annotation" id="wd-student-annotation"/>
+<label htmlFor="wd-radio-student-annotation">Student Annotation</label><br/>
+
+<input type="radio" name="radio-file-upload" id="wd-file-upload"/>
+<label htmlFor="wd-radio-file-upload">File Uploads</label>
+
+            </td>
+          </tr>
+
+           <tr>
+            <td align="right" valign="top">
+              <label htmlFor="wd-assign-to">Assign to</label>
+            </td>
+            <td>
+               <select id="wd-wd-assign-to-everyone" defaultValue="Everyone">
+               <option value="Everyone">Everyone</option>
+               </select>
+            </td>
+          </tr>
+
+          <tr>
+              <td align="right" valign="top">
+              <label htmlFor="wd-due-date">Due</label>
+            </td>
+            <td>
+              <input type="date" id="wd-due-date" />
+            </td>
+              
+          </tr>
+
+          <tr>
+              <td align="right" valign="top">
+              <label htmlFor="wd-available-from">Available from</label>
+            </td>
+              <td>
+              <input type="date" id="wd-available-from" />
+              </td>
+          </tr>
+
+          <tr>
+              <td align="right" valign="top">
+              <label htmlFor="wd-available-until">Until</label>
+            </td>
+            <td>
+              <input type="date" id="wd-available-until" />
+              </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
---
+++ app/(Kambaz)/Dashboard/page.tsx
@@ -0,0 +1,115 @@
+import Link from "next/link";
+import Image from "next/image";
+export default function Dashboard() {
+  return (
+    <div id="wd-dashboard">
+      <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
+      <h2 id="wd-dashboard-published">Published Courses (12)</h2> <hr />
+      <div id="wd-dashboard-courses">
+        <div className="wd-dashboard-course">
+          <Link href="/Courses/1234" className="wd-dashboard-course-link">
+            <Image src="/images/reactjs.jpg" width={200} height={150} alt="React JS course cover" />
+            <div>
+              <h5> CS1234 React JS </h5>
+              <p className="wd-dashboard-course-title">
+                Full Stack software developer
+              </p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
+
+         <div className="wd-dashboard-course">
+          <Link href="/Courses/4567" className="wd-dashboard-course-link">
+            <Image src="/images/reactjs.jpg" width={200} height={150} alt="React JS course cover" />
+            <div>
+              <h5> CS4567 JavaScript </h5>
+              <p className="wd-dashboard-course-title">
+                Full Stack software developer
+              </p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
+
+         <div className="wd-dashboard-course">
+          <Link href="/Courses/6789" className="wd-dashboard-course-link">
+            <Image src="/images/reactjs.jpg" width={200} height={150} alt="React JS course cover" />
+            <div>
+              <h5> CS6789 Programming Paradigm Design </h5>
+              <p className="wd-dashboard-course-title">
+                Full Stack software developer
+              </p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
+
+         <div className="wd-dashboard-course">
+          <Link href="/Courses/7980" className="wd-dashboard-course-link">
+            <Image src="/images/reactjs.jpg" width={200} height={150} alt="React JS course cover" />
+            <div>
+              <h5> CS7980 Database Managment Systems </h5>
+              <p className="wd-dashboard-course-title">
+                Back end software developer
+              </p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
+
+         <div className="wd-dashboard-course">
+          <Link href="/Courses/2345" className="wd-dashboard-course-link">
+            <Image src="/images/reactjs.jpg" width={200} height={150} alt="React JS course cover" />
+            <div>
+              <h5> CS2345 Angular </h5>
+              <p className="wd-dashboard-course-title">
+                Front end software developer
+              </p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
+
+         <div className="wd-dashboard-course">
+          <Link href="/Courses/1013" className="wd-dashboard-course-link">
+            <Image src="/images/docker.jpg" width={200} height={150} alt="Docker & Containers course cover" />
+            <div>
+              <h5> CS1013 Docker & Containers </h5>
+              <p className="wd-dashboard-course-title">
+                Containerization, images, and orchestration basics.
+              </p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
+
+         <div className="wd-dashboard-course">
+          <Link href="/Courses/1014" className="wd-dashboard-course-link">
+            <Image src="/images/algorithms.jpg" width={200} height={150} alt="Algorithms course cover" />
+            <div>
+              <h5> CS1014 Algorithms </h5>
+              <p className="wd-dashboard-course-title">
+                Sorting, searching, trees, and complexity analysis.
+              </p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
+
+         <div className="wd-dashboard-course">
+          <Link href="/Courses/1015" className="wd-dashboard-course-link">
+            <Image src="/images/uiux.jpg" width={200} height={150} alt="UI/UX Design course cover" />
+            <div>
+              <h5> CS1015 UI/UX</h5>
+              <p className="wd-dashboard-course-title">
+                Design principles, prototyping, and user research.
+              </p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
+      </div>
+      
+    </div>
+);}
