4bf38de813b1c445f665815eee585412ab779e22
Ashwin <105104146+AshwinGurumurthy@users.noreply.github.com>
2025-12-13 01:44:11 -0500

    small bug fix

--- app/(Kambaz)/Courses/[cid]/People/Table/page.tsx
+++ app/(Kambaz)/Courses/[cid]/People/Table/page.tsx
@@ -1,18 +1,36 @@
 "use client"
-import React, { useState } from "react";
+import { useState, useEffect } from "react";
 import { useParams } from "next/navigation";
 import { Table } from "react-bootstrap";
 import { FaUserCircle } from "react-icons/fa";
 //import * as db from "../../../../Database";
 import PeopleDetails from "../Details";
 import Link from "next/link";
+import * as client from "../../../client";
 
 export default function PeopleTable({ users = [], fetchUsers }: { users?: any[]; fetchUsers: () => void; }) {
     
-    //const { cid } = useParams(); 
+    const { cid } = useParams(); 
     //const { users, enrollments } = db;
     const [showDetails, setShowDetails] = useState(false);
   const [showUserId, setShowUserId] = useState<string | null>(null);
+
+  const [usersList, setUsersList] = useState(users);
+
+  useEffect(() => {
+    const getUsers = async () => {
+      if (users.length === 0) {
+        setUsersList(await client.findUsersForCourse(cid as string));
+      }
+    };
+    getUsers();
+  }, [users]);
+
+  useEffect(() => {
+  if (users.length > 0) {
+    setUsersList(users);
+  }
+}, [users]);
 
  return (
   <div id="wd-people-table">
@@ -30,7 +48,7 @@ export default function PeopleTable({ users = [], fetchUsers }: { users?: any[];
      <tr><th>Name</th><th>Login ID</th><th>Section</th><th>Role</th><th>Last Activity</th><th>Total Activity</th></tr>
     </thead>
     <tbody>
-  {users
+  {usersList
     .map((user) => (
       <tr key={user._id}>
         <td className="wd-full-name text-nowrap">
