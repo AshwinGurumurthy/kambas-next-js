29b650b459d707f38febc7445e0756d625afe2c5
Ashwin <105104146+AshwinGurumurthy@users.noreply.github.com>
2026-04-02 13:12:49 -0400

    integrated pazza project from group repo to this

--- app/(Kambaz)/Courses/[cid]/Piazza/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Piazza/page.tsx
@@ -1,6 +1,9 @@
 export default function Piazza() {
-  return (
-    <div>
-      <h2>Piazza</h2>
-    </div>
-);}
+  
+  const { currentUser } = useSelector(
+    (state: storeType) => state.accountReducer
+  );
+  if (currentUser) return redirect("/Pazza/Class/RS101");
+  else return redirect("/Account/Signin?redirect=/Pazza/Class/RS101");
+
+}
